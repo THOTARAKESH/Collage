c90c973227a5bc77c99a7bda3cb830a88278135e
mipreamble <imohammed23@gmail.com>
2014-07-15 10:44:35 +0530

    Enable Crashlytics

--- app/src/main/java/com/imran/collage/MyActivity.java
+++ app/src/main/java/com/imran/collage/MyActivity.java
@@ -2,6 +2,7 @@ package com.imran.collage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
 
 
 public class MyActivity extends Activity {
@@ -9,6 +10,7 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
         setContentView(R.layout.activity_my);
     }
 }
